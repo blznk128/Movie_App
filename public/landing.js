d8066fd500de89f99faa208e713fee6665d5f788
Benny Borunda <blznk128@yahoo.com>
2020-03-02 13:53:44 -0700

    added card

--- public/landing.js
+++ public/landing.js
@@ -1,16 +1,5 @@
 const movieName = $("#movie")
-
-// $("#searchMovie").on("click", function() {
-//     event.preventDefault()
-//     const urlSearch = "https://www.omdbapi.com/?t=" + movieName.val().trim() + "&y=&plot=short&apikey=trilogy";
-//     $.ajax({
-//         url: urlSearch,
-//         method: "GET"
-//       }).then(function(response) {
-//         console.log(response);
-//         console.log(response.Title)
-//       });
-// })
+const movieSummary = $("#summary")
 
 function searchMovie() {
     event.preventDefault()
@@ -21,5 +10,7 @@ function searchMovie() {
       }).then(function(response) {
         console.log(response);
         console.log(response.Title)
+        console.log(response.Plot)
+        movieSummary.append(response.Plot)
       });
 }
